f2a06f0bb2206b2d5ab972f2d2541b8ef38dca2b
Ede Meijer <ede.meijer88@gmail.com>
2017-05-15 23:38:53 +0200

    Create features and labels in numpy format from the tweets

--- prepare_data.py
+++ prepare_data.py
@@ -1,8 +1,9 @@
+import html
 import json
 import os
 import urllib.request as req
-import html
 
+import numpy as np
 from unidecode import unidecode
 
 # Thanks to this guy who did the hard work of collecting all Trump tweets!
@@ -87,15 +88,66 @@ def count_chars(tweets):
 
 
 def get_char_exclude_list(tweets):
-    counts = count_chars(tweets)
-    return [char for char, num in counts.items() if num < MIN_CHAR_OCCURRENCES]
+    """
+    Get a list of characters that have too little occurrences and should be excludes from the data set
+    """
+    return [char for char, num in count_chars(tweets).items() if num < MIN_CHAR_OCCURRENCES]
 
 
 def exclude_tweets_with_rare_chars(tweets):
+    """
+    Exclude tweets that contain characters with too little overall occurrences
+    """
     excludes = get_char_exclude_list(tweets)
     return [tweet for tweet in tweets if not any(char in tweet for char in excludes)]
 
 
-all_tweets = exclude_tweets_with_rare_chars(get_all_tweets())
-unique_chars = sorted(char for char, _ in count_chars(all_tweets).items())
+def get_char_feature(tweet, chars):
+    """
+    Given a tweet and a character list, determine the 0-based integer class for every character in the tweet and return
+    the list of classes.
+    """
+    return [chars.index(char) for char in tweet.lower()]
 
+
+def get_capitalization_feature(tweet):
+    """
+    Given a tweet, create a binary feature of whether a character is capitalized. Crucial for genuine Trump tweets.
+    """
+    return [0 if char == char.lower() else 1 for char in tweet]
+
+
+all_tweets = exclude_tweets_with_rare_chars(get_all_tweets())
+print("got all tweets, creating features and labels")
+
+# Unique chars in lower case. We will add an extra binary input for capitalization.
+unique_chars = sorted(char for char, _ in count_chars([t.lower() for t in all_tweets]).items())
+
+# Create the numpy array for all features and labels
+# There are 2 features in total. Dimensions are [num_examples x max_time_steps x num_features]
+features = np.zeros([len(all_tweets), max(len(tweet) + 1 for tweet in all_tweets), 2], dtype=int)
+lengths = np.zeros([len(all_tweets), 1], dtype=int)
+labels = np.zeros_like(features)
+
+for i in range(len(all_tweets)):
+    tweet = all_tweets[i]
+    num_steps = len(tweet)
+
+    char_feature = get_char_feature(tweet, unique_chars)
+    capitalization_feature = get_capitalization_feature(tweet)
+
+    features[i, :num_steps, 0] = char_feature
+    features[i, :num_steps, 1] = capitalization_feature
+    lengths[i, 0] = num_steps
+
+    labels[i, :num_steps - 1, 0] = char_feature[1:]
+    labels[i, num_steps - 1, 0] = len(unique_chars)
+    labels[i, :num_steps - 1, 1] = capitalization_feature[1:]
+
+np.save(CACHE_DIR + '/features.npy', features)
+np.save(CACHE_DIR + '/lengths.npy', lengths)
+np.save(CACHE_DIR + '/labels.npy', labels)
+
+# Save the list of unique characters
+with open(CACHE_DIR + '/settings.json', 'w') as file:
+    json.dump({'chars': unique_chars}, file)
